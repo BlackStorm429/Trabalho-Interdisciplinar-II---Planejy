7be5b501f8e7351e4b2a057a4b95c6c6bb71a607
Lara Souza <115436942+BlackStorm429@users.noreply.github.com>
2023-05-12 13:29:05 -0300

    Add files via upload

---
+++ Planejy/PlanejyService/src/main/java/dao/ArtigoDAO.java
@@ -0,0 +1,417 @@
+package dao;
+
+import model.Artigo;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Classe ArtigoDAO que herda a superclasse DAO - Data Access Object
+ * Conexao com o BD e' feita pela classe Pai
+ * 
+ * Hierarquia de chamada: Aplicacao -> Service -> DAO -> Model
+ * Aqui sao construidas as query's SQL para acesso ao BD
+ * 
+ * A classe Artigo serve apenas para consulta e nao possui POST
+ * 
+ * @method construtor
+ * @method finalize
+ * @method get
+ * @method getAll
+ */
+public class ArtigoDAO extends DAO {
+	/**
+	 * Construtor padrao que referencia a classe Pai
+	 */
+	public ArtigoDAO() {
+		super();
+		conectar();
+	}
+
+	/**
+	 * Fechar conexao com o BD
+	 */
+	public void finalize() {
+		close();
+	}
+
+	/**
+	 * Metodo GET para selecionar um artigo atraves de sua CHAVE
+	 * 
+	 * @see Artigo.java
+	 * @print erro se existir
+	 * @param chave Chave Primaria da tabela
+	 * @return Objeto Artigo contendo os dados necessarios para consulta ou vazio se
+	 *         um erro for gerado
+	 */
+	public Artigo get(int chave, String tokenUsuario) {
+		Artigo artigo = null;
+		try {
+			// Conexao
+			PreparedStatement stmt;
+			ResultSet rs;
+			String sql = "SELECT * FROM artigo WHERE chave = ?";
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setInt(1, chave);
+			rs = stmt.executeQuery();
+			// se algo for retornado, chamar o construtor
+			if (rs.next()) {
+				clique(tokenUsuario, chave);
+				double notas[] = getAvaliacao(rs.getInt("chave"));
+				artigo = new Artigo(rs.getInt("chave"), rs.getString("imagem"), rs.getString("imagem_alt"),
+						rs.getString("titulo"), rs.getString("conteudo"), rs.getString("resumo"),
+						rs.getString("autor"), rs.getDate("dia").toLocalDate(),
+						notas[0], (int) notas[1],
+						getNotaUsuario(tokenUsuario, rs.getInt("chave")));
+			}
+			// fechar a conexao
+			stmt.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return artigo;
+	}
+
+	public Artigo getAll(String tokenUsuario) {
+		Artigo artigo = new Artigo();
+		try {
+			// Conexao
+			PreparedStatement stmt;
+			ResultSet rs;
+			String sql = "";
+
+			// Criar dicionario para traducao de classificacao do artigo
+			sql += "SELECT * FROM classificacao_usuario ";
+			sql += "WHERE id_usuario = (SELECT id FROM usuario WHERE token = ? )";
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, tokenUsuario);
+			rs = stmt.executeQuery();
+			rs.next();
+			String nome[] = { "Trabalho", "Estudo", "Faculdade", "Curso", "Vida noturna", "Descanso", "Dia-a-dia",
+					"Social", "Social Profissional", "saude" };
+			int nota[] = {
+					rs.getInt("trabalho"),
+					rs.getInt("estudo"),
+					rs.getInt("faculdade"),
+					rs.getInt("curso"),
+					rs.getInt("vida_noturna"),
+					rs.getInt("descanso"),
+					rs.getInt("dia_a_dia"),
+					rs.getInt("social"),
+					rs.getInt("social_profissional"),
+					rs.getInt("saude")
+			};
+			Artigo.normalizarNotas(nota, nome);
+
+			// Recuperar registros dos profissionais
+			sql = "";
+			sql += "SELECT A.*, string_agg(B.tipo, ',') AS classificacao ";
+			sql += "FROM artigo AS A ";
+			sql += "INNER JOIN tipo_de_usuario_do_artigo AS B ";
+			sql += "ON A.chave = B.chave_artigo ";
+			sql += "GROUP BY A.chave ";
+			sql += "ORDER BY A.chave";
+			stmt = getConnection().prepareStatement(sql);
+			rs = stmt.executeQuery();
+
+			// Para cada string retornada, adicionar a pilha
+			while (rs.next()) {
+				double notas[] = getAvaliacao(rs.getInt("chave"));
+				Artigo p = new Artigo(rs.getInt("chave"), rs.getString("imagem"), rs.getString("imagem_alt"),
+						rs.getString("titulo"), rs.getString("conteudo"), rs.getString("resumo"),
+						rs.getString("autor"), rs.getDate("dia").toLocalDate(),
+						notas[0], (int) notas[1], rs.getString("classificacao"), usuarioJaEntrou(tokenUsuario, rs.getInt("chave")));
+				p.notaFinal(nome, nota);
+				artigo.add(p);
+			}
+
+			artigo.ordenar();
+
+			// fechar a conexao
+			stmt.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return artigo;
+	}
+
+	public double[] getAvaliacao(int chave_artigo) {
+		// Inicializacao de valores
+		double totalNotas = 0;
+		double numNotas = 0;
+		double notaFinal = 0;
+		try {
+			// Conexao
+			String sql = "SELECT * FROM Entrega_Artigo WHERE chave_artigo = ? AND avaliacao IS NOT NULL";
+			PreparedStatement stmt = getConnection().prepareStatement(sql);
+			stmt.setInt(1, chave_artigo);
+			ResultSet rs = stmt.executeQuery();
+			// Para cada registro atualizar valores
+			while (rs.next()) {
+				totalNotas += rs.getInt("avaliacao");
+				numNotas++;
+			}
+			// Calcular nota
+			if (numNotas > 0) {
+				notaFinal = totalNotas / numNotas;
+			}
+			// Fechar conexao
+			stmt.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			notaFinal = -1;
+		}
+		double result[] = new double[2];
+		result[0] = notaFinal;
+		result[1] = numNotas;
+		return result;
+	}
+
+	public int getNotaUsuario(String tokenUsuario, int chave_artigo) {
+		// Inicializacao de valores
+		int notaUsuario = 0;
+		try {
+			// Conexao
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql = "";
+			sql += "SELECT avaliacao FROM Entrega_Artigo WHERE chave_artigo = ? ";
+			sql += "AND id_usuario = (SELECT id FROM usuario WHERE token = ? AND avaliacao is NOT NULL ) ";
+			PreparedStatement stmt = getConnection().prepareStatement(sql);
+			stmt.setInt(1, chave_artigo);
+			stmt.setString(2, tokenUsuario);
+			ResultSet rs = stmt.executeQuery();
+			// Para cada registro atualizar valores
+			if (rs.next()) {
+				notaUsuario = rs.getInt("avaliacao");
+			}
+			// Fechar conexao
+			st.close();
+			stmt.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return notaUsuario;
+	}
+
+	public boolean usuarioJaEntrou(String tokenUsuario, int chave_artigo) {
+		// Falso ate se provar o contrario
+		boolean jaEntrou = false;;
+		try {
+			// Conexao
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql = "";
+			sql += "SELECT avaliacao FROM Entrega_Artigo WHERE chave_artigo = ? ";
+			sql += "AND id_usuario = (SELECT id FROM usuario WHERE token = ? ) ";
+			PreparedStatement stmt = getConnection().prepareStatement(sql);
+			stmt.setInt(1, chave_artigo);
+			stmt.setString(2, tokenUsuario);
+			ResultSet rs = stmt.executeQuery();
+			// Para cada registro atualizar valores
+			if (rs.next()) {
+				jaEntrou = true;
+			}
+			// Fechar conexao
+			st.close();
+			stmt.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return jaEntrou;
+	}
+
+	public boolean avaliar(String tokenUsuario, int chave, int nota) {
+		// False ate se provar o contrario
+		boolean status = false;
+		try {
+			// objetos de conexao
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql;
+
+			sql = "UPDATE Entrega_artigo ";
+			sql += "SET avaliacao = ?, cliques = (cliques + 1) ";
+			sql += "WHERE id_usuario = (SELECT id FROM usuario WHERE token = ? ) ";
+			sql += "AND chave_artigo = ?" ;
+
+			PreparedStatement stmt = getConnection().prepareStatement(sql);
+			stmt.setInt(1, nota);
+			stmt.setString(2, tokenUsuario);
+			stmt.setInt(3, chave);
+
+			// Execucao
+			stmt.executeUpdate();
+
+			// Fechar conexao
+			st.close();
+			stmt.close();
+			// Deu tudo certo!
+			status = atualizarClassificacaoUsuario(tokenUsuario, chave);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return status;
+	}
+
+	public boolean tempo(String tokenUsuario, int chave) {
+		// False ate se provar o contrario
+		boolean status = false;
+		try {
+			// objetos de conexao
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql;
+
+			sql = "UPDATE Entrega_artigo ";
+			sql += "SET tempo = (tempo + 1) ";
+			sql += "WHERE id_usuario = (SELECT id FROM usuario WHERE token = ? ) ";
+			sql += "AND chave_artigo = ?";
+
+			PreparedStatement stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, tokenUsuario);
+			stmt.setInt(2, chave);
+
+			// Execucao
+			stmt.executeUpdate();
+
+			// Fechar conexao
+			st.close();
+			stmt.close();
+			// Deu tudo certo!
+			status = atualizarClassificacaoUsuario(tokenUsuario, chave);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return status;
+	}
+
+	public boolean clique(String tokenUsuario, int chave) {
+		// False ate se provar o contrario
+		boolean status = false;
+		try {
+			// objetos de conexao
+			PreparedStatement stmt;
+			String sql;
+			ResultSet rs;
+
+			// Testar se ja existe avaliacao
+			boolean existe = true;
+			sql = "SELECT * FROM Planejy.Entrega_artigo ";
+			sql += "WHERE id_usuario = (SELECT id FROM usuario WHERE token = ? ) ";
+			sql += "AND chave_artigo = ?";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, tokenUsuario);
+			stmt.setInt(2, chave);
+			rs = stmt.executeQuery();
+
+
+			if (!rs.next()) {
+				existe = false;
+			}
+
+			// Se existir UPDATE, se nao INSERT
+			if (existe) {
+				sql = "UPDATE Entrega_artigo ";
+				sql += "SET cliques = (cliques + 1) ";
+				sql += "WHERE id_usuario = (SELECT id FROM usuario WHERE token = ? ) ";
+				sql += "AND chave_artigo = ?";
+			} else {
+				sql = "INSERT INTO Entrega_artigo (id_usuario, chave_artigo, cliques) ";
+				sql += "VALUES ((SELECT id FROM planejy.usuario WHERE token = ? ), ?, 1)";
+			}
+
+			// Execucao
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, tokenUsuario);
+			stmt.setInt(2, chave);
+
+			stmt.executeUpdate();
+
+			// Fechar conexao
+			stmt.close();
+			// Deu tudo certo!
+			status = atualizarClassificacaoUsuario(tokenUsuario, chave);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return status;
+	}
+
+	public boolean atualizarClassificacaoUsuario(String tokenUsuario, int chave_artigo) {
+		// False ate se provar o contrario
+		boolean status = false;
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql;
+			ResultSet rs;
+
+			// Recuperar a quantidade de classificacoes do profissional
+			sql = "SELECT count(chave_artigo) FROM tipo_de_usuario_do_artigo ";
+			sql += "WHERE chave_artigo = ?";
+			PreparedStatement stmt = getConnection().prepareStatement(sql);
+			stmt.setInt(1, chave_artigo);
+			
+			rs = stmt.executeQuery();
+			rs.next();
+			int numClassificacoes = rs.getInt("count");
+
+			// Recuperar a classe das classificacoes/* */
+			sql = "SELECT tipo FROM tipo_de_usuario_do_artigo ";
+			sql += "WHERE chave_artigo = ?";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setInt(1, chave_artigo);
+
+			rs = stmt.executeQuery();
+			String classificacoes[] = new String[numClassificacoes];
+			int i = 0;
+			String tmp;
+			while (rs.next()) {
+				classificacoes[i] = rs.getString("tipo");
+				tmp = "";
+				for (int j = 0; j < classificacoes[i].length(); j++) {
+					if (classificacoes[i].charAt(j) == '-') {
+						tmp += "_";
+					} else if (classificacoes[i].charAt(j) == ' ') {
+						tmp += "_";
+					} else {
+						tmp += classificacoes[i].charAt(j);
+					}
+				}
+				classificacoes[i] = tmp;
+				i++;
+			}
+
+			// Atualizar a classificacao do usuario
+			sql = "UPDATE classificacao_usuario ";
+			sql += "SET ";
+			for (i = 0; i < numClassificacoes; i++) {
+				sql += classificacoes[i] + " = (" + classificacoes[i] + " + 1)";
+				if (i < (numClassificacoes - 1)) {
+					sql += ", ";
+				} else {
+					sql += " ";
+				}
+			}
+			sql += "WHERE id_usuario = (SELECT id FROM usuario WHERE token = ? ) ";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, tokenUsuario);
+
+			// Execucao
+			stmt.executeUpdate();
+
+			// Fechar conexao
+			st.close();
+			stmt.close();
+			// Deu tudo certo!
+			status = true;
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		return status;
+	}
+}
---
+++ Planejy/PlanejyService/src/main/java/dao/UsuarioDAO.java
@@ -0,0 +1,512 @@
+package dao;
+
+import model.Usuario;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.security.*;
+import java.math.*;
+
+/**
+ * Classe UsuarioDAO que herda a superclasse DAO - Data Access Object
+ * Conexao com o BD e' feita pela classe Pai
+ * 
+ * Hierarquia de chamada: Aplicacao -> Service -> DAO -> Model
+ * Aqui sao construidas as query's SQL para acesso ao BD
+ * 
+ * @method construtor
+ * @method finalize
+ * @method md5
+ * @method get
+ * @method login
+ * @method insert
+ * @method update
+ * @method confirmarEmail
+ * @method mudarSenhaToke
+ */
+public class UsuarioDAO extends DAO {
+	/**
+	 * Construtor padrao que referencia a classe Pai
+	 */
+	public UsuarioDAO() {
+		super();
+		conectar();
+	}
+
+	/**
+	 * Fechar conexao com o BD
+	 */
+	public void finalize() {
+		close();
+	}
+
+	/**
+	 * Metodo de criptografia em Hash md5
+	 * 
+	 * O tamanho da string gerada e' definida no toString do return
+	 * 
+	 * @print erro se existir
+	 * @param s String a ser criptografada
+	 * @return String criptografada ou "erro"
+	 */
+	public static String md5(String s) {
+		try {
+			MessageDigest m;
+			m = MessageDigest.getInstance("MD5");
+			m.update(s.getBytes(), 0, s.length());
+			return (new BigInteger(1, m.digest()).toString(16));
+		} catch (NoSuchAlgorithmException e) {
+			System.err.println(e.getMessage());
+		}
+		return "erro";
+	}
+
+	/**
+	 * Metodo GET para retornar os dados do usuario guardados no BD
+	 * os dados do usuario serao recuperados atraves do Token de acesso unico do
+	 * usuario que e' gerado durante o login
+	 * 
+	 * @see Usuario.java
+	 * @print erro se existir
+	 * @param token token unico de acesso do usuario
+	 * @return objeto Usuario construido
+	 */
+	public Usuario get(String token) {
+		// Objeto vazio para popular
+		Usuario usuario = new Usuario();
+		try {
+			// Conexao
+			String sql = "SELECT * FROM usuario WHERE token = ?";
+
+			PreparedStatement stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, token);
+			ResultSet rs = stmt.executeQuery();
+
+			// Se receber, popular
+			if (rs.next()) {
+				usuario = new Usuario(rs.getInt("id"), rs.getString("nome"), rs.getDate("nascimento"),
+						rs.getString("nick"), rs.getString("senha"), rs.getString("email"),
+						rs.getString("genero"), rs.getString("token"));
+			}
+			// Fim de conexao
+			stmt.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return usuario;
+	}
+
+	/**
+	 * Metodo GET para excluir um usuario atraves do seu token e id
+	 * 
+	 * @print erro se existir
+	 * @param token token unico de acesso do usuario
+	 * @param id    primary key do usuario
+	 * @return objeto Usuario construido
+	 */
+	public boolean delete(String token, String id) {
+		// false ate se provar o contrario
+		boolean status = false;
+		try {
+			// Conexao
+			String sql = "DELETE FROM usuario WHERE token = ? AND id = ?";
+
+			PreparedStatement stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, token);
+			stmt.setInt(2, Integer.parseInt(id));
+			stmt.executeUpdate();
+
+			// Fim de conexao
+			stmt.close();
+			// Deu tudo certo!
+			status = true;
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return status;
+	}
+
+	/**
+	 * Metodo LOGIN para comparar se email e senha estao corretos e em seguida
+	 * registrar um token de acesso unico no banco de dados
+	 * A senha sera criptografada em md5
+	 * 
+	 * @see md5
+	 * @print erro se existir
+	 * @param email
+	 * @param senha
+	 * @param token para validacao do login
+	 * @return id do usuario logado
+	 */
+	public int login(String email, String senha, String token) {
+		// Criptografia
+		senha = md5(senha);
+		// Nao encontrado ate se provar o contrario
+		int id = -1;
+		try {
+			// Conexao
+			String sql = "SELECT id FROM usuario WHERE email = ? AND senha = ?";
+
+			PreparedStatement stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, email);
+			stmt.setString(2, senha);
+			ResultSet rs = stmt.executeQuery();
+
+			// Se retornar algo salvar
+			if (rs.next()) {
+				id = (rs.getInt("id"));
+			} else {
+				id = -1;
+			}
+			
+			// Se Id for encontrado efetuar o login e registrar o Token de acesso unico
+			if (id != -1) {
+				// Nova Conexao para registro
+				sql = "UPDATE usuario SET token = ? WHERE id = ?";
+
+				stmt = getConnection().prepareStatement(sql);
+				stmt.setString(1, token);
+				stmt.setInt(2, id);
+				stmt.executeQuery();
+
+				// Fim da conexao
+			}
+
+			// Fim de conexao
+			stmt.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return id;
+	}
+
+	/**
+	 * Metodo POST para registrar um novo usuario no Banco de Dados
+	 * 
+	 * E importante dizer que os dados registrados nao correspondem a todos os dados
+	 * existentes do usuario, demais dados devem utilizar o metodo update
+	 * 
+	 * A senha sera criptografada em md5 para depois o objeto Usuario ser cadastrado
+	 * 
+	 * @see md5
+	 * @print erro se existir
+	 * @param nome
+	 * @param nick
+	 * @param senha sem criptografia
+	 * @param email
+	 * @return Objeto JSON com o status de constraint do BD
+	 */
+	public String insert(String nome, String nick, String senha, String email) {
+		// Objeto JSON com o status da constraint
+		String result = "";
+		// Criptografia da senha
+		senha = md5(senha);
+		try {
+			// objetos de conexao
+			PreparedStatement stmt;
+			String sql;
+			ResultSet rs;
+
+			// Validacao de Nick
+			sql = "SELECT nick FROM usuario WHERE nick = ?";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, nick);
+			rs = stmt.executeQuery();
+
+			if (!rs.next()) {
+				result += ("{ \"Usuario\": [{ \"nick\":true , ");
+			} else {
+				result += ("{ \"Usuario\": [{ \"nick\":false , ");
+			}
+
+			// Validacao de Email
+			sql = "SELECT email FROM usuario WHERE email = ?";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, email);
+			rs = stmt.executeQuery();
+
+			if (!rs.next()) {
+				result += ("\"email\":true , ");
+			} else {
+				result += ("\"email\":false , ");
+			}
+
+			// Insercao propriamente dita
+			sql = "INSERT INTO usuario (nome, nick, senha, email) VALUES (?, ?, ?, ?)";
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, nome);
+			stmt.setString(2, nick);
+			stmt.setString(3, senha);
+			stmt.setString(4, email);
+			stmt.execute();
+			
+
+			// Cadastros subsequentes
+			// Cadastro na tabela de classificacao
+			/*
+			sql = "INSERT INTO classificacao_usuario (id_usuario) VALUES ((SELECT id FROM usuario WHERE email = ?))";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, email);
+			stmt.execute();
+			*/
+
+			// Fim de conexao
+			result += ("\"sucesso\":true } ] }");
+			stmt.close();
+		} catch (Exception e) {
+			result += ("\"sucesso\":false } ] }");
+			System.err.println(e.getMessage());
+		}
+		return result;
+	}
+	
+
+	/**
+	 * Metodo UPDATE para atualizar os dados de um usuario no Banco de Dados
+	 * 
+	 * O metodo utiliza o token de acesso unico do usuario para validar a sua
+	 * entrada Mas tambem utiliza o ID, por mesmo que redundante mas para seguranca
+	 * da validacao
+	 * 
+	 * Os dados do usuario a serem atualizado sao recebidos atraves da string 'body'
+	 * que sera parseada dentro do construtor da classe Usuario
+	 * 
+	 * @see Usuario.java
+	 * @print erro se existir
+	 * @param token token de validacao unico do usuario
+	 * @param id
+	 * @param body  dados a serem atualizados
+	 * @return Objeto JSON com o status de constraint do BD
+	 */
+	public String update(String token, int id, String body) {
+		// Objeto JSON com o status da constraint
+		String result = "";
+		// Criacao de um usuario a ser populado
+		Usuario usuario = new Usuario(body);
+		try {
+			// Conexao
+			PreparedStatement stmt;
+			String sql;
+			ResultSet rs;
+
+			// Validacao de Nick
+			sql = "SELECT nick FROM planejy.usuario WHERE nick = ?";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, usuario.getNick());
+			rs = stmt.executeQuery();
+
+			if (!rs.next()) {
+				result += ("{ \"Usuario\": [{ \"nick\":true , ");
+			} else {
+				result += ("{ \"Usuario\": [{ \"nick\":false , ");
+			}
+
+			// Validacao de Email
+			sql = "SELECT email FROM planejy.usuario WHERE email = ?";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, usuario.getEmail());
+			rs = stmt.executeQuery();
+
+			if (!rs.next()) {
+				result += ("\"email\":true , ");
+			} else {
+				result += ("\"email\":false , ");
+			}
+
+			// Update propriamente dito
+			sql = "UPDATE planejy.usuario SET email = ?, nome = ?, nascimento = ?, nick = ?, genero = ? ";
+			sql += "WHERE id = ? AND token = ?";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, usuario.getEmail());
+			stmt.setString(2, usuario.getNome());
+			stmt.setDate(3, new Date(usuario.getNascimento().getTime()));
+			stmt.setString(4, usuario.getNick());
+			stmt.setString(5, usuario.getGenero());
+			stmt.setInt(6, id);
+			stmt.setString(7, token);
+			stmt.executeUpdate();
+			
+			result += ("\"sucesso\":true } ] }");
+
+			// fim de conexao
+			stmt.close();
+		} catch (Exception e) {
+			result += ("\"sucesso\":false } ] }");
+			System.err.println(e.getMessage());
+		}
+		// limpeza da variavel
+		usuario = null;
+		return result;
+	}
+
+	/**
+	 * Metodo para confirmacao de existencia de email para mudanca de senha
+	 * Caso o email seja valido sera inserido no Banco de Dados um token de
+	 * recuperacao de senha
+	 * 
+	 * @see mudarSenhaToken
+	 * @print erro se exitir
+	 * @param token token de recuperacao de senha
+	 * @param email email a ser verificado
+	 * @return id do usuario a ter a senha alterada
+	 */
+	public int confirmarEmail(String token, String email) {
+		// Usuario nao encontrado ate se provar o contrario
+		int id = -1;
+		try {
+			// Recuperar o ID equivalente ao email e Conexao
+			PreparedStatement stmt;
+			ResultSet rs;
+			String sql = "SELECT id FROM planejy.usuario WHERE email = ?";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, email);
+			rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				id = (rs.getInt("id"));
+			} else {
+				id = -1;
+			}
+
+			// Se o email existir o token de recuperacao de senha sera registrado a conta
+			if (id != -1) {
+				// Nova conexao
+				sql = "UPDATE planejy.usuario SET token = ? WHERE id = ?";
+
+				stmt = getConnection().prepareStatement(sql);
+				stmt.setString(1, token);
+				stmt.setInt(2, id);
+				stmt.executeQuery();
+			}
+
+			// Fim de conexao
+			stmt.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return id;
+	}
+
+	/**
+	 * Metodo para efetivar a mudanca de senha do usuario
+	 * 
+	 * O metodo fara a mudanca de senha atraves da validacao do Token de recuperacao
+	 * gerado. A senha entregue sera criptografada em MD5 antes de ser efetivamente
+	 * armazenada
+	 * 
+	 * @see confirmarEmail
+	 * @see md5
+	 * @print erro se existir
+	 * @param token token de recuperacao de senha
+	 * @param senha nova senha a ser inserida
+	 * @return id do usuario que teve a senha alterada
+	 */
+	public int mudarSenhaToken(String token, String senha) {
+		// Usuario nao encontrado ate se provar o contrario
+		int id = -1;
+		// criptografia da senha
+		senha = md5(senha);
+		try {
+			// Recuperacao do ID atraves do token gerado e conexao
+			PreparedStatement stmt;
+			ResultSet rs;
+			String sql = "";
+			sql += "SELECT id FROM planejy.usuario WHERE token = ?";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, token);
+			rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				id = (rs.getInt("id"));
+			} else {
+				id = -1;
+			}
+			// Fim de conexao
+
+			// Efetivar a mudanca de senha
+			if (id != -1) {
+				// Conexao
+				sql = "UPDATE planejy.usuario SET senha = ? WHERE id = ?";
+
+				stmt = getConnection().prepareStatement(sql);
+				stmt.setString(1, senha);
+				stmt.setInt(2, id);
+				stmt.executeQuery();
+			}
+
+			stmt.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return id;
+	}
+
+	/**
+	 * Metodo para incrementar a categoria de um usuario
+	 * 
+	 * O metodo fara o incremento no banco de dados de todas as categorias enviadas
+	 * 
+	 * @print erro se existir
+	 * @param token token de recuperacao de senha
+	 * @param categorias categorias a serem incrementadas
+	 * @return true se bem sucedido
+	 */
+	public boolean addCategoria(String token, String[] categorias) {
+		// False ate se provar o contrario
+		boolean result = false;
+		try {
+			// Recuperacao do ID atraves do token gerado e conexao
+			int id = -1;
+			PreparedStatement stmt;
+			ResultSet rs;
+			String sql = "";
+			sql += "SELECT id FROM planejy.usuario WHERE token = ?";
+
+			stmt = getConnection().prepareStatement(sql);
+			stmt.setString(1, token);
+			rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				id = (rs.getInt("id"));
+			} else {
+				id = -1;
+			}
+
+			// Efetivar a atualizacao dos dados
+			if (id != -1) {
+				// Conexao, construcao e execucao
+				sql = "UPDATE planejy.classificacao_usuario SET ";
+				for (int i = 0; i < categorias.length; i++) {
+					sql += categorias[i];
+					sql += " = ";
+					sql += categorias[i];
+					sql += " +1";
+					if (i < categorias.length - 1) {
+						sql += ", ";
+					}
+				}
+				sql += " WHERE id_usuario = ?";
+
+				stmt = getConnection().prepareStatement(sql);
+				stmt.setInt(1, id);
+				stmt.executeQuery();
+
+				result = true;
+			}
+
+			stmt.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return result;
+	}
+}
